f4eebd3fcc7b1f0ac38b47455a1411d69323f6e4
Bibek <admin@thedeveloperservice.com>
2021-09-14 11:35:34 +0545

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# animated-slider
+This is a animated slider using Html, Css and JavaScript
+
+#### Demo: https://codersgyan.github.io/animated-slider/
+
+🙏 If you find this repo helpful then don't forget to give a start ❇️ to this repository. :)
---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $('.slider').slick({
+        autoplay:true,
+        fade:true,
+        autoplaySpeed:'6000',
+        dots:true,
+        appendDots:'.slider-dots',
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+    });
+
+
+
+
+
+
+});
